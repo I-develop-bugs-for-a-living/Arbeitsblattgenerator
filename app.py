65399cde5f5b31b0d79b5b66cf70cbc26c86f831
I-develop-bugs-for-a-living <jan.buehne@outlook.com>
2022-02-03 11:27:54 +0100

    Initial commit

---
+++ app.py
@@ -0,0 +1,37 @@
+import random
+
+def generateHTML(content):
+    template = f"""
+        <h1>Math Arbeitsblatt</h1>
+        <p>Berechne alle Aufgaben so gut du kannst:</p>
+        <h3>Die Matheaufgaben</h3>
+    """
+    for i in content:
+        template = template + f"<p>{i}</p>\n"
+    with open("index.html", "w") as file:
+        file.write(template)
+
+def generateMath():
+    operatorsEasy = ["+", "-"]
+    operatorsHard = ["*", ":"]
+    mathContent = []
+    for i in range(15):
+        op = random.choice(operatorsEasy)
+        num1 = random.randint(1, 100)
+        num2 = random.randint(1, 100)
+        mathContent.append(f"{num1} {op} {num2} = ")
+    for i in range(15):
+        op = random.choice(operatorsHard)
+        num1 = random.randint(1, 10)
+        if op == ":":
+            num2 = num1 * random.randint(1,5)
+            num1, num2 = num2, num1
+        else:
+            num2 = random.randint(1, 10)
+        mathContent.append(f"{num1} {op} {num2} = ")
+    return mathContent
+
+    print(mathContent)
+
+content = generateMath()
+generateHTML(content)
